d8dd9acb3e55d405d604373fe586d572e70b7391
Rudra Sarkar <rds8rds@gmail.com>
2024-02-06 02:28:35 +0600

    2nd try

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    // <> react fragment 
+    <> 
+      <h2>Hello World</h2>
+      <button>Increment</button>
+      <span style = {{marginLeft: '5px'}}>0</span>
+    </>
+  );
+}
+
+export default App;

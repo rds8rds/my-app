8966951d04e3792b3f00eb832ea8486780b8d642
Rudra Sarkar <rds8rds@gmail.com>
2024-02-08 19:44:19 +0600

    simplifying in class 11

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
 import React, { Component } from "react";
-import Counter from "./components/counter.component";
-import Navbar from "./components/navbar.component";
-
-// functional component dia amra render() ke use korte parbo na
 
 class App extends Component {
-  state = {
-    counters: [1, 2, 3, 4],
-    handleCounters: [1],
-  };
-  handleRemove = () => {
-    const counters = [...this.state.handleCounters];
-    counters.pop();
-    this.setState({ handleCounters: counters });
-  };
-
-  handleAdd = () => {
-    const counters = [...this.state.handleCounters];
-    counters.push(1);
-    this.setState({ handleCounters: counters });
-  };
-
   render() {
     return (
       // <> react fragment
       <>
-        <Navbar />
-        <button onClick={this.handleAdd} className="btn btn-primary">
-          {" "}
-          Add{" "}
-        </button>
-
-        {this.state.handleCounters.map((counter, index) => (
-          <>
-            <br />
-            <Counter onRemove={this.handleRemove} key={index} />
-          </>
-        ))}
-
-        {/* {this.state.counters.map((counter, index) => (
-          <h1 key={index}>{counter}</h1>
-        ))} */}
+        <h1>Hello World</h1>
+        <p>Hello World</p>
       </>
     );
   }

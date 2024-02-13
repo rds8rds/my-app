5ff4a1428d2b3eed69e05bcd6cae6716edf86abd
Rudra Sarkar <rds8rds@gmail.com>
2024-02-13 23:50:57 +0600

    class 14 complite but can't pass the showNonZeroCounters to Navbar class

--- src/App.js
+++ src/App.js
@@ -1,72 +1,15 @@
 import React, { Component } from "react";
-import Counter from "./components/counter.component";
+import Counters from "./components/counters.component";
 import Navbar from "./components/navbar.component";
 
-// functional component dia amra render() ke use korte parbo na
-
 class App extends Component {
-  state = {
-    handleCounters: [1, 2, 3, 4, 5],
-    activeCart: 0,
-  };
-  handleRemove = () => {
-    const counters = [...this.state.handleCounters];
-    counters.pop();
-    this.setState({ handleCounters: counters });
-  };
-
-  handleAdd = () => {
-    const counters = [...this.state.handleCounters];
-    counters.push(1);
-    this.setState({ handleCounters: counters });
-  };
-
-  counter = () => {
-    return this.state.handleCounters.length;
-  };
-
-  activeCartInc = () => {
-    const curr = this.state.activeCart;
-    this.setState({ activeCart: curr + 1 });
-    console.log(this.state.activeCart);
-  };
-
-  activeCartDec = () => {
-    const curr = this.state.activeCart;
-    this.setState({ activeCart: curr - 1 });
-    console.log(this.state.activeCart);
-  };
-
   render() {
     return (
-      // <> react fragment
       <>
-        <Navbar handleAdd={this.handleAdd} activeCart={this.state.activeCart} />
-        {this.state.handleCounters.map((counter, index) => (
-          <>
-            <br />
-            <Counter
-              onRemove={this.handleRemove}
-              key={index}
-              activeCartInc={this.activeCartInc}
-              activeCartDec={this.activeCartDec}
-            />
-          </>
-        ))}
-
-        {/* {this.state.counters.map((counter, index) => (
-          <h1 key={index}>{counter}</h1>
-        ))} */}
+        <Navbar />
+        <Counters />
       </>
     );
   }
 }
-
 export default App;
-
-// new nav bar
-// modified counter component with css and button
-// map function implementation on array
-// new add button implementation
-// *** home work ***
-// props object passing from Apps to conter as onRemove
--- src/components/counter.component.js
+++ src/components/counter.component.js
@@ -1,73 +1,35 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-  };
-
-  handleDecrement = () => {
-    if (this.state.count > 0) this.setState({ count: this.state.count - 1 });
-  };
-
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  disableDecresebutton = () => {
-    return this.state.count === 0;
-  };
-
-  handleRemove = () => {
-    if (this.props.onRemove) {
-      this.props.onRemove();
-    }
-  };
-
-  handleClick = (param1, param2, flag) => {
-    param1();
-    if (flag && this.state.count == 0) param2();
-    else if (!flag && this.state.count == 1) param2();
+  handle = () => {
+    this.props.handleIncrement(this.props.id);
   };
 
   render() {
+    const { id, value, handleIncrement, handleDecrement, handleRemove } =
+      this.props;
     return (
-      <div className="d-flex flex-row">
-        <span className="p-2"> Quantity </span>
-        <button
-          disabled={this.disableDecresebutton()} /* full function passing  */
-          className="p-2 btn btn-primary"
-          onClick={() =>
-            this.handleClick(
-              this.handleDecrement,
-              this.props.activeCartDec,
-              false
-            )
-          }
-        >
-          -
-        </button>
-        <span className="p-2"> {this.state.count} </span>
-        <button
-          className="p-2 btn btn-primary"
-          onClick={() =>
-            this.handleClick(
-              this.handleIncrement,
-              this.props.activeCartInc,
-              true
-            )
-          }
-        >
-          +
-        </button>
-        <button
-          style={{ marginLeft: "5px" }} /* taking spaces */
-          onClick={this.handleRemove}
-          className="btn btn-primary"
-        >
-          {" "}
-          Remove{" "}
-        </button>
-      </div>
+      <>
+        <div style={{ margin: "20px " }}>
+          <button
+            onClick={() => handleIncrement(id)}
+            className="btn btn-primary"
+          >
+            +
+          </button>
+          <h4>{this.props.value}</h4>
+          <button
+            onClick={() => handleDecrement(id)}
+            className="btn btn-primary"
+          >
+            -
+          </button>
+          <button onClick={() => handleRemove(id)} className="btn btn-allert">
+            {" "}
+            Remove{" "}
+          </button>
+        </div>
+      </>
     );
   }
 }
